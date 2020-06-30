561587205485314b333e889024de765b2d85402e
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-30 17:50:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-    planeteer_calls.find do |element|
+    planeteer_calls.each do |element|
     valid_calls.include? element
     p element
   end
